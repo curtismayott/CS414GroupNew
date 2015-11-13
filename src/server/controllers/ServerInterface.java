c769c418a0190d1e660c2cee54a0a417c204bb25
Jim <gazawayj@rams.colostate.edu>
2015-11-13 16:29:00 -0700

    Added server interface for android

--- src/server/controllers/ServerInterface.java
+++ src/server/controllers/ServerInterface.java
@@ -6,6 +6,6 @@ import java.rmi.RemoteException;
 /**
  * Created by Jim on 11/13/2015.
  */
-public interface ServerInterface extends Remote {
+public interface ServerInterface extends Remote{
     public String print() throws RemoteException;
 }
--- src/server/controllers/ServerRemote.java
+++ src/server/controllers/ServerRemote.java
@@ -1,19 +1,37 @@
 package server.controllers;
 
+import server.objects.Order;
+import server.objects.Person;
+
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 /**
  * Created by Jim on 11/13/2015.
  */
-public class ServerRemote extends UnicastRemoteObject implements ServerInterface{
+public class ServerRemote extends UnicastRemoteObject implements ServerInterface, AndroidServerInterface{
 
     protected ServerRemote() throws RemoteException {
         super();
     }
 
     @Override
+    public boolean addNewOrder(Order order) throws RemoteException {
+        return false;
+    }
+
+    @Override
+    public boolean addNewCustomer(Person customer) throws RemoteException {
+        return false;
+    }
+
+    @Override
+    public boolean editCustomer(Person customer) throws RemoteException {
+        return false;
+    }
+
+    @Override
     public String print() throws RemoteException {
-        return "It works!";
+        return "It Works!";
     }
 }
